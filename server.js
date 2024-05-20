3527393b7cab19c14951dc31094d1483761694eb
Tony Randazzo <t.randazzo@teamcompany.local>
2024-05-20 12:55:24 +0200

    secondo commit

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+
+const groups = {};
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -14,10 +17,24 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('User Disconnected');
+    Object.keys(groups).forEach(group => {
+      if (groups[group].includes(socket.id)) {
+        groups[group] = groups[group].filter(userId => userId !== socket.id);
+        io.to(group).emit('user left', { userId: socket.id });
+      }
+    });
   });
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', { msg: msg, userId: socket.id });
+  socket.on('create group', (groupName) => {
+    groups[groupName] = [socket.id];
+    socket.join(groupName);
+    socket.emit('group created', groupName);
+    console.log(`User ${socket.id} created and joined group ${groupName}`);
+  });
+
+
+  socket.on('chat message', (msgData) => {
+    io.to(msgData.group).emit('chat message', { msg: msgData.msg, userId: socket.id });
   });
 });
 
