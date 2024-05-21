150ab63d3a1bafdcd5d51fc708aea4ec556fe13f
Tony Randazzo <t.randazzo@teamcompany.local>
2024-05-21 12:59:29 +0200

    gruppi

--- server.js
+++ server.js
@@ -9,15 +9,36 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/groups.html', (req, res) => {
+  res.sendFile(__dirname + '/groups.html');
+});
+
 io.on('connection', (socket) => {
   console.log('User Connected');
+  io.emit('chat message', { msg: 'Online', userId: 'system' });
 
   socket.on('disconnect', () => {
     console.log('User Disconnected');
+    io.emit('chat message', { msg: 'Offline', userId: 'system' });
   });
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', { msg: msg, userId: socket.id });
+  socket.on('join group', (group) => {
+    socket.join(group);
+    io.to(group).emit('chat message', { msg: `User ${socket.id} joined ${group}`, userId: 'system' });
+  });
+
+  socket.on('leave group', (group) => {
+    socket.leave(group);
+    io.to(group).emit('chat message', { msg: `User ${socket.id} left ${group}`, userId: 'system' });
+    socket.emit('left group', group); 
+  });
+
+  socket.on('chat message', (data) => {
+    if (data.group) {
+      io.to(data.group).emit('chat message', { msg: data.msg, userId: socket.id });
+    } else {
+      io.emit('chat message', { msg: data.msg, userId: socket.id });
+    }
   });
 });
 
